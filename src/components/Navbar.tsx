3e87642c861e9787ad1b9e3295cb08551978b066
Ben Engelberg <benjaminsengelberg@gmail.com>
2022-08-10 09:09:54 -0700

    front end progress, transactions container, navbar, css styling, graph

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,14 @@ import './Navbar.css'
 
 const Navbar = () => {
     return (
-        <nav id='navbar'>
+        <nav id='navbar' className="navbar">
             <div id='el'>
-              <button>Overview</button>     
-              <div id="account">
-                {`Username`}
-              </div>       
+              <button className="navbar">Overview</button>     
+              <button className="navbar">Transactions</button>     
+              <button className="navbar">Budgets</button>     
+              <div id="account" className="navbar">
+                <a href="url" id="username">{`Username`}</a>
+              </div> 
             </div>
         </nav>
     )
