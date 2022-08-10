509dca5842f9c4a0b9d0cb606459ee4f3dc0b516
Keyla Koizumi Nishimura <kkoizuminishimura@gmail.com>
2022-08-09 17:02:00 -0700

    setup bare bones MainContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
+import MainContainer from './containers/MainContainer'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Budget Tracker
+        <MainContainer></MainContainer>
+       
       </header>
     </div>
   );
---
+++ src/containers/MainContainer.tsx
@@ -0,0 +1,60 @@
+import React, {useEffect, useState} from 'react';
+
+const MainContainer = () => {
+
+  //state: Expending and Budge info from user
+
+  //either we get the use data and saved it all in the state data
+  const [data, setData] = useState([]);
+
+  //OR we split the data from the request into separate states
+  const [expenses, setExpenses] = useState([]);
+  const [budget, setBudget] = useState([]);
+
+  //form to post later on
+  const [formExpenses, setFormExpenses] = useState([]);
+
+  //get data from user when component mounts
+  // useEffect(() => {
+  //   getData()
+  // }, []);
+  const getData = () => {
+    fetch('/getData', {
+      method: 'GET',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin' 
+    })
+      .then((res) => res.json)
+      .then((res) => setData(res))
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const updateExpenses = () => {
+    fetch('/updateExpenses', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: formExpenses,
+      }),
+      credentials: 'same-origin' 
+    })
+      .then((res) => res.json)
+      .then((res) => setExpenses(res))
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  return (
+    <div>
+      {'hello'}
+    </div>
+  );
+}
+export default MainContainer;
